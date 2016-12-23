93a0638a30bce16cadfc58c841547b6ee03e674c
Rauzer <D.V.Kramer@student.hhs.nl>
2016-12-23 14:11:06 +0100

    Entity and VisualObject are now together again

--- Engine KB-01/Engine KB-01/Entity.h
+++ Engine KB-01/Engine KB-01/Entity.h
@@ -3,15 +3,19 @@
 
 #include "MatrixWrapper.h"
 
+//Virtual class, every gameObject is an entity and has certain base properties. 
+//But depending on their typing, they add onto this
 
 class Entity
 {
 public:
+	//Returns the Entity's position
 	virtual MatrixWrapper* getPosition();
 	virtual void Initialize() =0;
 	Entity();
 	~Entity();
 protected:
+	//Every Entity has a position in the world
 	MatrixWrapper* Position;
 private:
 
--- Engine KB-01/Engine KB-01/VisualObject.h
+++ Engine KB-01/Engine KB-01/VisualObject.h
@@ -9,21 +9,47 @@
 class VisualObject : public Entity
 {
 public:
+	//Constructor
 	VisualObject();
+
+	//Destructor
 	~VisualObject();
+
+	//Initializes the VisualObject
 	void Initialize();
+
+	//Gets the model, the meshname
 	std::string getModel();
+
+	//Gets the texture, the texturename
 	std::string getTexture();
+
+	//Gets the subSets
 	EDWORD getSubSets();
+
+	//Sets the model
 	void setModel(std::string newMeshname);
+
+	//Sets the texture
 	void setTexture(std::string newTextname);
+
+	//Sets the Subset
 	void setSubSet(int newDWORD);
+
+	//Sets the Position of the VisualObject
 	void setPosition(MatrixWrapper* newPosition);
+
+	//The draw logic for a visual object, since it has visual components (a model/texture) that needs to be drawn
 	void draw(Renderer* myDirectXRenderer);
 
 private:
+	//Model/meshname
 	std::string meshname;
+
+	//Texture
 	std::string textname;
+
+	//Subset, which parts of the mesh needs to be used
 	EDWORD subsets;
 };
 
